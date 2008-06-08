a74de75bb942d2371f50caabaf8a705da94e9856
bromine <bromine@e85cb8aa-f74c-0410-bc79-b65f13e76ef0>
2008-06-08 20:44:49 +0000

    manualRunner.php patched!

--- manualRunner.php
+++ manualRunner.php
@@ -21,17 +21,21 @@
     echo "<form method='get' action='createManSuite.php' target='blank'>";
     echo "<table>";
     echo "<tr>";
-    echo "<td>Vælg browser:</td>";
+    echo "<td>".$lh->GetText('Choose your current browser').":</td>";
     echo "<td>";
     $browser=$_GET['browser'];
-    $result=$dbh->select('TRM_browser',"","*");
+    $result=$dbh->select('TRM_browser',"ORDER BY browsername","*");
     $numreports=mysql_numrows($result);
     echo "<select name='browser'>";
     echo "<option value=''>".$lh->getText('Choose')."</option>";
     for($i=0;$i<$numreports;$i++){
       echo "<option value='".mysql_result($result,$i,"ID")."'";
     
-      if($browser==mysql_result($result,$i,"ID")){echo ' selected="selected"';}
+      if ($OS==mysql_result($result,$i,"ID") ||
+          preg_match( "/${browsersuggest}/i", mysql_result($result,$i,"browsername")))
+      {
+          echo ' selected="selected"';
+      }
       
       echo ">".mysql_result($result,$i,"browsername")."</option>";
     }
@@ -40,29 +44,33 @@
     
     echo "</tr>";
     echo "<tr>";
-    echo "<td>Vælg OS:</td>";
+    echo "<td>".$lh->GetText('Choose your current OS').":</td>";
     echo "<td>";
     
     $OS=$_GET['OS'];
-    $result=$dbh->select('TRM_OS',"","*");
+    $result=$dbh->select('TRM_OS',"ORDER BY OSname","*");
     $numreports=mysql_numrows($result);
     echo "<select name='OS'>";
     echo "<option value=''>".$lh->getText('Choose')."</option>";
     for($i=0;$i<$numreports;$i++){
       echo "<option value='".mysql_result($result,$i,"ID")."'";
     
-      if($OS==mysql_result($result,$i,"ID")){echo ' selected="selected"';}
-      
+      if ($OS==mysql_result($result,$i,"ID") ||
+          preg_match( "/${platformsuggest}/i", mysql_result($result,$i,"OSname")))
+      {
+          echo ' selected="selected"';
+      }
+
       echo ">".mysql_result($result,$i,"OSname")."</option>";
     }
     echo "</select>";
     echo "</td>";
     echo "</tr>";
     echo "<tr>";
-    echo "<td>Vælg test:</td>";
+    echo "<td>".$lh->GetText('Choose test').":</td>";
     echo "<td>";    
     
-    $result= $dbh->sql("SELECT * FROM TRM_design_manual_test WHERE p_id=$p_id");
+    $result= $dbh->sql("SELECT * FROM TRM_design_manual_test WHERE p_id=$p_id ORDER BY name");
     $numreports=mysql_numrows($result);
     echo "<select name='td_id'>";
     echo "<option value=''>".$lh->getText('Choose')."</option>";
@@ -77,11 +85,11 @@
     echo "</td>";
     echo "</tr>";
     echo "<tr>";
-    echo "<td>Site to test:</td>";
+    echo "<td>".$lh->GetText('Choose environment').":</td>";
     
     echo "<td>";
     
-    $siteresult = $dbh->select('TRM_projects_has_sites',"WHERE p_id=$p_id","*");
+    $siteresult = $dbh->select('TRM_projects_has_sites',"WHERE p_id=$p_id ORDER BY sitetotest","*");
     while($row = mysql_fetch_array($siteresult)){
       	$sitetotests[]=$row['sitetotest'];
     }

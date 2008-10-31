243468ff0f5967f240c1fb88196ba3c035324479
bromine <bromine@e85cb8aa-f74c-0410-bc79-b65f13e76ef0>
2008-10-31 21:40:12 +0000

    Added word like tiny_mce features

--- Bromine/editProjects.php
+++ Bromine/editProjects.php
@@ -2,6 +2,45 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
+<!-- TinyMCE -->
+<script type="text/javascript" src="../tmr/jscripts/tiny_mce/tiny_mce.js"></script>
+<script type="text/javascript">
+	tinyMCE.init({
+		// General options
+		mode : "textareas",
+		theme : "advanced",
+		plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,inlinepopups",
+
+		// Theme options
+		theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+		theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+		theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+		theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
+		theme_advanced_toolbar_location : "top",
+		theme_advanced_toolbar_align : "left",
+		theme_advanced_statusbar_location : "bottom",
+		theme_advanced_resizing : true,
+
+		// Example word content CSS (should be your site CSS) this one removes paragraph margins
+		content_css : "css/word.css",
+
+		// Drop lists for link/image/media/template dialogs
+		template_external_list_url : "lists/template_list.js",
+		external_link_list_url : "lists/link_list.js",
+		external_image_list_url : "lists/image_list.js",
+		media_external_list_url : "lists/media_list.js",
+
+		// Replace values for the template plugin
+		template_replace_values : {
+			username : "Some User",
+			staffid : "991234"
+		}
+	});
+</script>
+
+<!-- /TinyMCE -->
+
+
         <?php include ('header.php'); ?>
         <link rel="stylesheet" type="text/css" href="style.css" />
     </head>
@@ -28,7 +67,7 @@
                     $viewdefectsurl = mysql_result($result, $i, "viewdefectsurl");
                     $adddefecturl = mysql_result($result, $i, "adddefecturl");
 
-                    echo "<fieldset style='width: 600px;' id='fs$name'>";
+                    echo "<fieldset style='width: 768px;' id='fs$name'>";
                     echo '<legend style="cursor: pointer; font-weight: bold;" onclick="'."$('$name').toggle(); $(which).value='$name'".'">'.$name.'</legend>';
                     echo "<div id='$name' style='display:none;'>"; 
                     echo "<table>";
@@ -104,7 +143,7 @@
                 }
                 
             ?> 
-            <fieldset style='width: 600px;'>
+            <fieldset style='width: 768px;'>
             <legend style="cursor: pointer; font-weight: bold;" onclick="$('addnew').toggle()"><?php echo $lh->getText('Add project') ?></legend>
             <div id="addnew" style='display: none;'>
                 <table>

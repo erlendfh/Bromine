5a8e742a7056d51ee9a8c223b6afc8ad85d50d9b
pvk <pvk@e85cb8aa-f74c-0410-bc79-b65f13e76ef0>
2009-05-13 12:38:11 +0000

    Added news_controller, that can read rss from both fisheye & jira... Should be used for at 'news' site, so uses can see updates in Bromine. try localhost/news

---
+++ app/controllers/news_controller.php
@@ -0,0 +1,14 @@
+<?php
+class NewsController extends AppController {
+
+	var $name = 'News';
+	var $helpers = array('Html', 'Form', 'Rss');
+	var $uses = array();
+
+	function index() {
+        
+
+    }
+	
+}
+?>

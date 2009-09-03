f115aa771a479a83515c22816f8b77dae9a6bdeb
jpp <jpp@e85cb8aa-f74c-0410-bc79-b65f13e76ef0>
2009-09-03 12:03:16 +0000

    Should now work on Linux.

--- app/webroot/install/index.php
+++ app/webroot/install/index.php
@@ -23,44 +23,7 @@
         <div id='messages'>
         </div>
         <?php
-        /*
-echo "<table border=\"1\">";
-echo "<tr><td>" .$_SERVER['argv'] ."</td><td>argv</td></tr>";
-echo "<tr><td>" .$_SERVER['argc'] ."</td><td>argc</td></tr>";
-echo "<tr><td>" .$_SERVER['GATEWAY_INTERFACE'] ."</td><td>GATEWAY_INTERFACE</td></tr>";
-echo "<tr><td>" .$_SERVER['SERVER_ADDR'] ."</td><td>SERVER_ADDR</td></tr>";
-echo "<tr><td>" .$_SERVER['SERVER_NAME'] ."</td><td>SERVER_NAME</td></tr>";
-echo "<tr><td>" .$_SERVER['SERVER_SOFTWARE'] ."</td><td>SERVER_SOFTWARE</td></tr>";
-echo "<tr><td>" .$_SERVER['SERVER_PROTOCOL'] ."</td><td>SERVER_PROTOCOL</td></tr>";
-echo "<tr><td>" .$_SERVER['REQUEST_METHOD'] ."</td><td>REQUEST_METHOD</td></tr>";
-echo "<tr><td>" .$_SERVER['REQUEST_TIME'] ."</td><td>REQUEST_TIME</td></tr>";
-echo "<tr><td>" .$_SERVER['QUERY_STRING'] ."</td><td>QUERY_STRING</td></tr>";
-echo "<tr><td>" .$_SERVER['DOCUMENT_ROOT'] ."</td><td>DOCUMENT_ROOT</td></tr>";
-echo "<tr><td>" .$_SERVER['HTTP_ACCEPT'] ."</td><td>HTTP_ACCEPT</td></tr>";
-echo "<tr><td>" .$_SERVER['HTTP_ACCEPT_CHARSET'] ."</td><td>HTTP_ACCEPT_CHARSET</td></tr>";
-echo "<tr><td>" .$_SERVER['HTTP_ACCEPT_ENCODING'] ."</td><td>HTTP_ACCEPT_ENCODING</td></tr>";
-echo "<tr><td>" .$_SERVER['HTTP_ACCEPT_LANGUAGE'] ."</td><td>HTTP_ACCEPT_LANGUAGE</td></tr>";
-echo "<tr><td>" .$_SERVER['HTTP_CONNECTION'] ."</td><td>HTTP_CONNECTION</td></tr>";
-echo "<tr><td>" .$_SERVER['HTTP_HOST'] ."</td><td>HTTP_HOST</td></tr>";
-echo "<tr><td>" .$_SERVER['HTTP_REFERER'] ."</td><td>HTTP_REFERER</td></tr>";
-echo "<tr><td>" .$_SERVER['HTTP_USER_AGENT'] ."</td><td>HTTP_USER_AGENT</td></tr>";
-echo "<tr><td>" .$_SERVER['HTTPS'] ."</td><td>HTTPS</td></tr>";
-echo "<tr><td>" .$_SERVER['REMOTE_ADDR'] ."</td><td>REMOTE_ADDR</td></tr>";
-echo "<tr><td>" .$_SERVER['REMOTE_HOST'] ."</td><td>REMOTE_HOST</td></tr>";
-echo "<tr><td>" .$_SERVER['REMOTE_PORT'] ."</td><td>REMOTE_PORT</td></tr>";
-echo "<tr><td>" .$_SERVER['SCRIPT_FILENAME'] ."</td><td>SCRIPT_FILENAME</td></tr>";
-echo "<tr><td>" .$_SERVER['SERVER_ADMIN'] ."</td><td>SERVER_ADMIN</td></tr>";
-echo "<tr><td>" .$_SERVER['SERVER_PORT'] ."</td><td>SERVER_PORT</td></tr>";
-echo "<tr><td>" .$_SERVER['SERVER_SIGNATURE'] ."</td><td>SERVER_SIGNATURE</td></tr>";
-echo "<tr><td>" .$_SERVER['PATH_TRANSLATED'] ."</td><td>PATH_TRANSLATED</td></tr>";
-echo "<tr><td>" .$_SERVER['SCRIPT_NAME'] ."</td><td>SCRIPT_NAME</td></tr>";
-echo "<tr><td>" .$_SERVER['REQUEST_URI'] ."</td><td>REQUEST_URI</td></tr>";
-echo "<tr><td>" .$_SERVER['PHP_AUTH_DIGEST'] ."</td><td>PHP_AUTH_DIGEST</td></tr>";
-echo "<tr><td>" .$_SERVER['PHP_AUTH_USER'] ."</td><td>PHP_AUTH_USER</td></tr>";
-echo "<tr><td>" .$_SERVER['PHP_AUTH_PW'] ."</td><td>PHP_AUTH_PW</td></tr>";
-echo "<tr><td>" .$_SERVER['AUTH_TYPE'] ."</td><td>AUTH_TYPE</td></tr>";
-echo "</table>";
-*/
+ 
     if ($_GET['page'] == 'step1' || $_GET['page'] == ''){
     /*
     lav en db forbindelse
@@ -69,7 +32,7 @@ echo "</table>";
     opret admin bruger
     slet denne install mappe
     */
-    $dbFile = getcwd(). '\\..\\..\\config\\database.php';
+    $dbFile = getcwd(). '/../../config/database.php';
     echo "<h1>This will install Bromine 3</h1><br>";
     echo "<h2>Step 1: Database infomation</h2>";
     $_SESSION['steps'] = 5;
@@ -120,18 +83,18 @@ echo "</table>";
             
             $fh = fopen($dbFile, 'w') or die("can't open file");
             $stringData = "<?php
-class DATABASE_CONFIG {
-	var \$default = array(
-		'driver' => 'mysql',
-		'persistent' => false,
-		'host' => '$host',
-		'login' => '$login',
-		'password' => '$password',
-		'database' => '$database',
-		'prefix' => '',
-	);
-}
-?>";
+                            class DATABASE_CONFIG {
+                            	var \$default = array(
+                            		'driver' => 'mysql',
+                            		'persistent' => false,
+                            		'host' => '$host',
+                            		'login' => '$login',
+                            		'password' => '$password',
+                            		'database' => '$database',
+                            		'prefix' => '',
+                            	);
+                            }
+                            ?".">";
             fwrite($fh, $stringData);
             fclose($fh);
             echo "<b>Database connection succesful.</b><br />";
@@ -146,7 +109,7 @@ class DATABASE_CONFIG {
     }
     elseif($_GET['page'] == 'step2'){
         echo "<h1>This will install Bromine 3</h1><br>";
-        echo "<h2>Step 2: Installing SQL dump</h2>";
+        echo "<h2>Step 2: Setting up database</h2>";
         function multiple_query($q, $link) {
             $tok = strtok($q, ";");
             while ($tok) {
@@ -165,7 +128,7 @@ class DATABASE_CONFIG {
         
         $db = @mysql_connect($host, $login, $password);
         if (!$db) {
-            die("<p><strong>Sorry, there was a problem with the installation.</strong> Check your Username/Password/Host. <span class='mysql-error'>(" . mysql_error() . ")</span></p>");
+            die("<p><strong>Sorry, could not connect to the MySQL server. <span class='mysql-error'>(" . mysql_error() . ")</span></p>");
         }
         
         $status = @mysql_select_db($database, $db);
@@ -185,7 +148,7 @@ class DATABASE_CONFIG {
         
         echo "<br /><b>Database succesful installed.</b><br />";
         echo "<b>Please delete /install dir.</b><br />";
-        $server = $_SERVER['SERVER_NAME'];
+        //$server = $_SERVER['SERVER_NAME'];
         
         echo "<br><br><h2>Login with this user:</h2>";
         echo "
@@ -195,7 +158,7 @@ class DATABASE_CONFIG {
             <dt>Password:</dt>
             <dd>admin</dd>
         </dl>";
-        echo "<br /><h2><a href='http://$server/configs/stateOfTheSystem'>Continue</a></h2>";
+        echo "<br /><h2><a href='/configs/stateOfTheSystem'>Continue</a></h2>";
         
         }
     ?>
